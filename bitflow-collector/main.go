3f4271f7033732d7aa7286c6d719a12fe13169c3
Anton Gulenko <anton.gulenko@googlemail.com>
2017-11-21 12:31:32 +0100

    Extracted part of the main packaged into reusable cmd_helper.

--- bitflow-collector/main.go
+++ bitflow-collector/main.go
@@ -4,47 +4,24 @@ import (
 	"flag"
 	"os"
 
-	"net/http"
-
-	log "github.com/sirupsen/logrus"
-	"github.com/antongulenko/go-bitflow"
-	"github.com/antongulenko/go-bitflow-pipeline"
-	"github.com/antongulenko/go-bitflow-pipeline/fork"
-	"github.com/antongulenko/go-bitflow-pipeline/http_tags"
+	"github.com/antongulenko/go-bitflow-collector/cmd_helper"
 	"github.com/antongulenko/golib"
-	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	os.Exit(do_main())
 }
 
-const restApiPathPrefix = "/api"
-
-var (
-	restApiEndpoint string
-	fileOutputApi   FileOutputFilterApi
-)
-
 func do_main() int {
 	print_metrics := flag.Bool("print-metrics", false, "Print all available metrics and exit")
 	print_pipeline := flag.Bool("print-pipeline", false, "Print the data collection pipeline and exit")
 	print_graph := flag.String("graph", "", "Create png-file for the collector-graph and exit")
 	print_graph_dot := flag.String("graph-dot", "", "Create dot-file for the collector-graph and exit")
-	var sink_flags golib.StringSlice
-	flag.Var(&sink_flags, "o", "Data sink(s) for outputting data")
-	var flagTags golib.KeyValueStringSlice
-	flag.Var(&flagTags, "tag", "All collected samples will have the given tags (key=value) attached.")
-	flag.StringVar(&restApiEndpoint, "api", "", "Enable REST API for controlling the collector. "+
-		"The API can be used to control tags and enable/disable file output.")
 
 	// Parse command line flags
-	f := bitflow.NewEndpointFactory()
-	f.RegisterGeneralFlagsTo(flag.CommandLine)
-	f.RegisterOutputFlagsTo(flag.CommandLine)
-	bitflow.RegisterGolibFlags()
-	flag.Parse()
-	golib.ConfigureLogging()
+	var cmd cmd_helper.CmdDataCollector
+	cmd.ParseFlags()
 	if flag.NArg() > 0 {
 		golib.Fatalln("Stray command line argument(s):", flag.Args())
 	}
@@ -52,26 +29,8 @@ func do_main() int {
 
 	// Configure the data collector pipeline
 	collector := createCollectorSource()
-	var p pipeline.SamplePipeline
+	p := cmd.MakePipeline()
 	p.Source = collector
-	if len(flagTags.Keys) > 0 {
-		p.Add(pipeline.NewTaggingProcessor(flagTags.Map()))
-	}
-	if restApiEndpoint != "" {
-		router := mux.NewRouter()
-		tagger := http_tags.NewHttpTagger(restApiPathPrefix, router)
-		fileOutputApi.Register(restApiPathPrefix, router)
-		server := http.Server{
-			Addr:    restApiEndpoint,
-			Handler: router,
-		}
-		// Do not add this routine to any wait group, as it cannot be stopped
-		go func() {
-			tagger.Error(server.ListenAndServe())
-		}()
-		p.Add(tagger)
-	}
-	add_outputs(&p, f, sink_flags)
 
 	// Print requested information
 	stop := false
@@ -102,62 +61,4 @@ func do_main() int {
 	}
 
 	return p.StartAndWait()
-}
-
-func add_outputs(p *pipeline.SamplePipeline, f *bitflow.EndpointFactory, outputStrings []string) {
-	outputs := create_outputs(f, outputStrings)
-	if len(outputs) == 1 {
-		set_sink(p, outputs[0])
-	} else {
-		p.Sink = new(bitflow.EmptyMetricSink)
-
-		// Create a multiplex-fork for all outputs
-		num := len(outputs)
-		builder := make(fork.MultiplexPipelineBuilder, num)
-		for i, sink := range outputs {
-			builder[i] = new(pipeline.SamplePipeline)
-			set_sink(builder[i], sink)
-		}
-		p.Add(&fork.MetricFork{
-			ParallelClose: true,
-			Distributor:   fork.NewMultiplexDistributor(num),
-			Builder:       builder,
-		})
-	}
-}
-
-func create_outputs(f *bitflow.EndpointFactory, outputs []string) []bitflow.MetricSink {
-	if len(outputs) == 0 {
-		outputs = []string{"box://-"} // Print to console as default
-	}
-	var sinks []bitflow.MetricSink
-	consoleOutputs := 0
-	for _, output := range outputs {
-		sink, err := f.CreateOutput(output)
-		sinks = append(sinks, sink)
-		golib.Checkerr(err)
-		if bitflow.IsConsoleOutput(sink) {
-			consoleOutputs++
-		}
-		if consoleOutputs > 1 {
-			golib.Fatalln("Cannot define multiple outputs to stdout")
-		}
-	}
-	return sinks
-}
-
-func set_sink(p *pipeline.SamplePipeline, sink bitflow.MetricSink) {
-	p.Sink = sink
-
-	// Add a filter to file outputs
-	if _, isFile := sink.(*bitflow.FileSink); isFile {
-		if restApiEndpoint != "" {
-			p.Add(&pipeline.SampleFilter{
-				Description: pipeline.String("Filter samples while no tags are defined via REST"),
-				IncludeFilter: func(sample *bitflow.Sample, header *bitflow.Header) (bool, error) {
-					return fileOutputApi.FileOutputEnabled, nil
-				},
-			})
-		}
-	}
 }
---
+++ cmd_helper/cmd.go
@@ -0,0 +1,160 @@
+package cmd_helper
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"sync"
+
+	bitflow "github.com/antongulenko/go-bitflow"
+	pipeline "github.com/antongulenko/go-bitflow-pipeline"
+	"github.com/antongulenko/go-bitflow-pipeline/fork"
+	"github.com/antongulenko/go-bitflow-pipeline/http_tags"
+	"github.com/antongulenko/golib"
+	"github.com/gorilla/mux"
+)
+
+const RestApiPathPrefix = "/api"
+
+type CmdDataCollector struct {
+	Endpoints         *bitflow.EndpointFactory
+	RestApiPathPrefix string
+
+	restApiEndpoint string
+	fileOutputApi   FileOutputFilterApi
+	outputs         golib.StringSlice
+	flagTags        golib.KeyValueStringSlice
+}
+
+func (c *CmdDataCollector) ParseFlags() {
+	flag.Var(&c.outputs, "o", "Data sink(s) for outputting data")
+	flag.Var(&c.flagTags, "tag", "All collected samples will have the given tags (key=value) attached.")
+	flag.StringVar(&c.restApiEndpoint, "api", "", "Enable REST API for controlling the collector. "+
+		"The API can be used to control tags and enable/disable file output.")
+
+	// Parse command line flags
+	c.Endpoints = bitflow.NewEndpointFactory()
+	c.Endpoints.RegisterGeneralFlagsTo(flag.CommandLine)
+	c.Endpoints.RegisterOutputFlagsTo(flag.CommandLine)
+	bitflow.RegisterGolibFlags()
+	flag.Parse()
+	golib.ConfigureLogging()
+}
+
+func (c *CmdDataCollector) MakePipeline() *pipeline.SamplePipeline {
+	// Configure the data collector pipeline
+	p := new(pipeline.SamplePipeline)
+	if len(c.flagTags.Keys) > 0 {
+		p.Add(pipeline.NewTaggingProcessor(c.flagTags.Map()))
+	}
+	if c.restApiEndpoint != "" {
+		router := mux.NewRouter()
+		tagger := http_tags.NewHttpTagger(RestApiPathPrefix, router)
+		c.fileOutputApi.Register(RestApiPathPrefix, router)
+		server := http.Server{
+			Addr:    c.restApiEndpoint,
+			Handler: router,
+		}
+		// Do not add this routine to any wait group, as it cannot be stopped
+		go func() {
+			tagger.Error(server.ListenAndServe())
+		}()
+		p.Add(tagger)
+	}
+	c.add_outputs(p)
+	return p
+}
+
+func (c *CmdDataCollector) add_outputs(p *pipeline.SamplePipeline) {
+	outputs := c.create_outputs()
+	if len(outputs) == 1 {
+		c.set_sink(p, outputs[0])
+	} else {
+		p.Sink = new(bitflow.EmptyMetricSink)
+
+		// Create a multiplex-fork for all outputs
+		num := len(outputs)
+		builder := make(fork.MultiplexPipelineBuilder, num)
+		for i, sink := range outputs {
+			builder[i] = new(pipeline.SamplePipeline)
+			c.set_sink(builder[i], sink)
+		}
+		p.Add(&fork.MetricFork{
+			ParallelClose: true,
+			Distributor:   fork.NewMultiplexDistributor(num),
+			Builder:       builder,
+		})
+	}
+}
+
+func (c *CmdDataCollector) create_outputs() []bitflow.MetricSink {
+	if len(c.outputs) == 0 {
+		c.outputs = []string{"box://-"} // Print to console as default
+	}
+	var sinks []bitflow.MetricSink
+	consoleOutputs := 0
+	for _, output := range c.outputs {
+		sink, err := c.Endpoints.CreateOutput(output)
+		sinks = append(sinks, sink)
+		golib.Checkerr(err)
+		if bitflow.IsConsoleOutput(sink) {
+			consoleOutputs++
+		}
+		if consoleOutputs > 1 {
+			golib.Fatalln("Cannot define multiple outputs to stdout")
+		}
+	}
+	return sinks
+}
+
+func (c *CmdDataCollector) set_sink(p *pipeline.SamplePipeline, sink bitflow.MetricSink) {
+	p.Sink = sink
+
+	// Add a filter to file outputs
+	if _, isFile := sink.(*bitflow.FileSink); isFile {
+		if c.restApiEndpoint != "" {
+			p.Add(&pipeline.SampleFilter{
+				Description: pipeline.String("Filter samples while no tags are defined via REST"),
+				IncludeFilter: func(sample *bitflow.Sample, header *bitflow.Header) (bool, error) {
+					return c.fileOutputApi.FileOutputEnabled, nil
+				},
+			})
+		}
+	}
+}
+
+type FileOutputFilterApi struct {
+	lock              sync.Mutex
+	FileOutputEnabled bool
+}
+
+func (api *FileOutputFilterApi) Register(pathPrefix string, router *mux.Router) {
+	router.HandleFunc(pathPrefix+"/file_output", api.handleRequest).Methods("GET", "POST", "PUT", "DELETE")
+}
+
+func (api *FileOutputFilterApi) handleRequest(w http.ResponseWriter, r *http.Request) {
+	api.lock.Lock()
+	oldStatus := api.FileOutputEnabled
+	newStatus := oldStatus
+	switch r.Method {
+	case "GET":
+	case "POST", "PUT":
+		newStatus = true
+	case "DELETE":
+		newStatus = false
+	}
+	api.FileOutputEnabled = newStatus
+	api.lock.Unlock()
+
+	var status string
+	if api.FileOutputEnabled {
+		status = "enabled"
+	} else {
+		status = "disabled"
+	}
+	status = "File output is " + status
+	if oldStatus != newStatus {
+		log.Println(status)
+	}
+	w.Write([]byte(status + "\n"))
+}
