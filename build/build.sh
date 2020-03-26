8c4032a8c4db6106c08ad96233bf90ab893952e7
Anton Gulenko <anton.gulenko@googlemail.com>
2020-03-26 16:30:09 +0100

    Small optimization in build.sh

--- build/build.sh
+++ build/build.sh
@@ -5,18 +5,18 @@ root=`readlink -f "$home/.."`
 test $# -ge 2 || { echo "Parameters: <container to build for (arm32v7/arm64v8/alpine)> <Go-mod-cache directory> <Build args (optional)>"; exit 1; }
 BUILD_TARGET="$1"
 BUILD_IMAGE="teambitflow/bitflow-collector-build:$BUILD_TARGET"
-BUILD_DIR="../src/build/_output/$BUILD_TARGET"
+BUILD_DIR="build/_output/$BUILD_TARGET"
+echo "Building into $BUILD_DIR"
 shift
 
 mod_cache_dir=$(readlink -f "$1/$BUILD_TARGET")
 echo "Using Go-mod-cache directory: $mod_cache_dir"
-MOUNT_GO_MOD_CACHE="-v $mod_cache_dir:/go"
 shift
 
 build_args="$@"
 
 # Build inside the container, but mount relevant directories to get access to the build results.
-docker run -ti $MOUNT_GO_MOD_CACHE -v "$root:/build/src" "$BUILD_IMAGE" \
+docker run -ti -v "$mod_cache_dir:/go" -v "$root:/build/src" "$BUILD_IMAGE" \
   sh -c "
     # Copy entire source-tree in order to make changes to go.mod/go.sum
     cp -r src build
@@ -27,6 +27,6 @@ docker run -ti $MOUNT_GO_MOD_CACHE -v "$root:/build/src" "$BUILD_IMAGE" \
     find -name go.sum -delete
 
     # Build the collector and plugins, put the outputs in the mounted source folder
-    go build -o $BUILD_DIR/bitflow-collector $build_args ./bitflow-collector
-    ./plugins/build-plugins.sh $BUILD_DIR/bitflow-collector-plugins $build_args
+    go build -o ../src/$BUILD_DIR/bitflow-collector $build_args ./bitflow-collector
+    ./plugins/build-plugins.sh ../src/$BUILD_DIR/bitflow-collector-plugins $build_args
   "
