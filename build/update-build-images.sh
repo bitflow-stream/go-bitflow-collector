42f7befa8bcc8318842f4f374168a21daa877910
Anton Gulenko <anton.gulenko@googlemail.com>
2020-03-29 23:07:11 +0200

    Fixed update script

--- build/update-build-images.sh
+++ build/update-build-images.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Build images
-docker build -t bitflowstream/golang-collector-build:debian -f collector-build-debian.Dockerfile .
-docker build -t bitflowstream/golang-collector-build:alpine -f collector-build-alpine.Dockerfile .
-docker build -t bitflowstream/golang-collector-build:arm32v7 -f collector-build-arm32v7.Dockerfile .
-docker build -t bitflowstream/golang-collector-build:arm64v8 -f collector-build-arm64v8.Dockerfile .
+docker build -t bitflowstream/golang-collector-build:debian -f debian-build.Dockerfile .
+docker build -t bitflowstream/golang-collector-build:alpine -f alpine-build.Dockerfile .
+docker build -t bitflowstream/golang-collector-build:arm32v7 -f arm32v7-build.Dockerfile .
+docker build -t bitflowstream/golang-collector-build:arm64v8 -f arm64v8-build.Dockerfile .
 
 # Push updated images
 docker push bitflowstream/golang-collector-build:debian
