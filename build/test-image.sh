bc01c1a1892317b49c2374cefb601f47df2d3c0d
Anton Gulenko <anton.gulenko@googlemail.com>
2020-04-01 10:34:58 +0200

    Fixing Docker container test script

--- build/test-image.sh
+++ build/test-image.sh
@@ -4,5 +4,9 @@ test $# = 1 || { echo "Need 1 parameter: image tag to test"; exit 1; }
 IMAGE="bitflowstream/bitflow-collector"
 TAG="$1"
 
+# Give the Docker daemon some time, since the image was just built
+echo "Waiting for 10 seconds..."
+sleep 10
+
 # Sanity check: image starts, outputs valid JSON, and terminates.
-docker run "$IMAGE:$TAG" -json-capabilities | tee /dev/stderr | jq . > /dev/null
+docker run "$IMAGE:$TAG" -json-capabilities | tee /dev/stderr | jq -ne inputs > /dev/null
